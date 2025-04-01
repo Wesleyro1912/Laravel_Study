1d05ac2ef8ca425adb508c671ef832f0608d598f
Wesley Rodrigues <127269265+Wesleyro1912@users.noreply.github.com>
2025-03-31 22:35:29 -0300

    00

--- Api-version12x/resources/views/users/create.blade.php
+++ Api-version12x/resources/views/users/create.blade.php
@@ -4,35 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources\css\app.css'])
     <title>Formulário de Cadastro</title>
 </head>
 <body>
-    <h1>Cadastrar Usuário</h1>
+    <div class="flex flex">
+        <h1>Cadastrar Usuário</h1>
 
-    @if (session('success'))
-        <p style="color: #086">
-            {{ session('success') }}
-        </p>
-    @endif
+        @if (session('success'))
+            <p style="color: #086">
+                {{ session('success') }}
+            </p>
+        @endif
 
-    @if (session('error'))
-        <p style="color: #f00">
-            {{ session('error') }}
-        </p>
-    @endif
+        @if (session('error'))
+            <p style="color: #f00">
+                {{ session('error') }}
+            </p>
+        @endif
 
-    <form action="{{ route('user.store')}}" method="POST">
-        @csrf
-        <label for="name">Nome</label>
-        <input type="text" name="name" id="name" placeholder="Nome completo do usuário" value="{{ old('name') }}" required><br><br>
+        <form action="{{ route('user.store')}}" method="POST">
+            @csrf
+            <label for="name">Nome</label>
+            <input type="text" name="name" id="name" placeholder="Nome completo do usuário" value="{{ old('name') }}" required><br><br>
 
-        <label for="email">E-mail</label>
-        <input type="email" name="email" id="email" placeholder="E-mail do usuário" value="{{ old('email') }}" required><br><br>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" placeholder="E-mail do usuário" value="{{ old('email') }}" required><br><br>
 
-        <label for="password">Senha</label>
-        <input type="password" name="password" id="password" placeholder="Senha do usuário" value="{{ old('password') }}" required><br><br>
+            <label for="password">Senha</label>
+            <input type="password" name="password" id="password" placeholder="Senha do usuário" value="{{ old('password') }}" required><br><br>
 
-        <button type="submit">Cadastrar</button>
-    </form>
+            <button type="submit">Cadastrar</button>
+        </form>
+    </div>
+    
 </body>
 </html>
