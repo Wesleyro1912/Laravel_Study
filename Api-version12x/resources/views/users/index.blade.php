072c3d6766a1e8de9c114dbca67658bc861af208
Wesley Rodrigues <127269265+Wesleyro1912@users.noreply.github.com>
2025-04-03 22:23:49 -0300

    03° commit

--- Api-version12x/resources/views/users/index.blade.php
+++ Api-version12x/resources/views/users/index.blade.php
@@ -9,7 +9,38 @@
             </div>
 
             <x-alert />
-            {{ dd($users) }}
-           
+            <div class="table-container">
+                <table class="table">
+                    <thead>
+                        <tr class="table-header">
+                            <th class="table-header">ID</th>
+                            <th class="table-header">Nome</th>
+                            <th class="table-header">E-mail</th>
+                            <th class="table-header center">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-body">
+                        @forelse ($users as $user)
+                            <tr class="table-row">
+                                <td class="table-cell">{{ $user->id }}</td>
+                                <td class="table-cell">{{ $user->name }}</td>
+                                <td class="table-cell">{{ $user->email }}</td>
+                                <td class="table-actions">
+                                    <a href="#" class="btn-primary">Visualizar</a>
+                                    <a href="#" class="btn-warning">Editar</a>
+                                    <a href="#" class="btn-danger">Apagar</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <div class="alert-error">
+                                Nenhum Usuário encontado!
+                            </div>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+            <div class="pagination">
+                {{ $users->links() }}
+            </div>
         </div>
 @endsection
